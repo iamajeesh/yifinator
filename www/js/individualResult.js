e6c43650d0a4e7c24e9a9052cf1cdde54bebe363
Francisco Quintero Linux Mint <frajaquico@gmail.com>
2014-12-07 04:09:51 -0500

    agregar libreria que permite mostrar videos de youtube

--- www/js/individualResult.js
+++ www/js/individualResult.js
@@ -1,6 +1,4 @@
-
-
-var individual = angular.module('individual',[]);
+var individual = angular.module('individual',['youtube-embed']);
 
 individual.controller('IndividualCtrl', function($scope,$location,$window,$rootScope,$http){
 
@@ -19,12 +17,14 @@ individual.controller('IndividualCtrl', function($scope,$location,$window,$rootS
     var ref = $window.open(movie,'_system','location=no');
   };
   
-  var url = 'https://yts.re/api/movie.json?id='+$rootScope.movieId+'';
-  //var url = 'https://yts.re/api/movie.json?id=5515';
+  //var url = 'https://yts.re/api/movie.json?id='+$rootScope.movieId+'';
+  var url = 'https://yts.re/api/movie.json?id=5515';
 
   $http.get(url).success(function(data){
     $scope.movieData = data;
     movie = $scope.movieData.TorrentUrl;
+    $scope.theBestVideo = $scope.movieData.YoutubeTrailerID;
   });
+
 
 });
