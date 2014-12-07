064652a771f7037dd7f1b40a12f9bdf02a4c7f3e
aprendizfc <richard11_71@outlook.com>
2014-12-07 05:15:48 -0500

    guarda

--- www/js/individualResult.js
+++ www/js/individualResult.js
@@ -18,6 +18,7 @@ individual.controller('IndividualCtrl', function($scope,$location,$window,$rootS
   $scope.openUrl = function(){
     var ref = $window.open(movie,'_system','location=no');
   };
+}
   
   var url = 'https://yts.re/api/movie.json?id='+$rootScope.movieId+'';
   //var url = 'https://yts.re/api/movie.json?id=5515';
